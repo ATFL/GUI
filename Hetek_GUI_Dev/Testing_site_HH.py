67810c5075818979106b26cc9e38cfb94c559173
Adithya Ravishankara <adiravishankara@gmail.com>
2019-06-12 11:56:01 -0700

    fd

--- Hetek_GUI_Dev/Testing_site_HH.py
+++ Hetek_GUI_Dev/Testing_site_HH.py
@@ -406,7 +406,7 @@ def collect_data(xVector,yVector):
                 linearActuator.retract()
     print('Data Capture Complete')
     combinedVector = np.column_stack((timeVector, dataVector))
-    for i in range(0:length(timeVector)):
+    for i in range(0,length(timeVector)):
         print(timeVector[i], ",", dataVector[i])
 
     # This section of code is used for generating the output file name. The file name will contain date/time of test, as well as concentration values present during test
