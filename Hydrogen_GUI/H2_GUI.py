0db4ef8ed7e68a55686ffb84886726ade8faf2ac
Adithya Ravishankara <adiravishankara@gmail.com>
2019-07-02 18:24:46 -0700

    sfd

--- Hydrogen_GUI/H2_GUI.py
+++ Hydrogen_GUI/H2_GUI.py
@@ -96,12 +96,14 @@ num_tests = len(methane_injection_conc)
 
 ##############################################
 
-fill_methane_time = [0,0,0]
+#fill_methane_time = [0,0,0]
+fill_methane_time = [0] * num_tests
 methane_correction_factor = 0.73#found it on MKS website
 methane_flow_rate = 10#what the value on the MFC is set to
 methane_flow_factor = 60/(500*methane_correction_factor*methane_flow_rate)
 
-fill_hydrogen_time =  [0,0,0]
+#fill_hydrogen_time =  [0,0,0]
+fill_hydrogen_time = [0] * num_tests
 hydrogen_correction_factor = 0.73#found it on MKS website
 hydrogen_flow_rate = 20#what the value on the MFC is set to
 hydrogen_flow_factor = 60/(500*hydrogen_correction_factor*hydrogen_flow_rate)
