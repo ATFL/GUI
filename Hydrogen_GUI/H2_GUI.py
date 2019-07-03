c805d5666ff942f6ab56df00ce6a31f38e8e7013
Adithya Ravishankara <adiravishankara@gmail.com>
2019-07-02 18:32:41 -0700

    m gfh

--- Hydrogen_GUI/H2_GUI.py
+++ Hydrogen_GUI/H2_GUI.py
@@ -111,7 +111,7 @@ hydrogen_correction_factor = 0.73#found it on MKS website
 hydrogen_flow_rate = 20#what the value on the MFC is set to
 hydrogen_flow_factor = 60/(500*hydrogen_correction_factor*hydrogen_flow_rate)
 
-for i in range(0, len(methane_injection_conc)-1):
+for i in range(0, len(hydrogen_injection_conc)-1):
     fill_methane_time[i] = int(methane_injection_conc[i]*methane_flow_factor)
     fill_hydrogen_time[i] = int(hydrogen_injection_conc[i]*hydrogen_flow_factor)
 #########################################################\
